2acf295982514e7b49fc63463a1a75c42a81c993
Crissy Cabrera <crissymichelle@proton.me>
2023-04-09 21:35:19 -1000

    10.5 object enhancements Exercise

---
+++ objectEnhancementsExerciseCabrera.js
@@ -0,0 +1,69 @@
+//refactor into ES2015
+// function createInstructor(firstName, lastName){
+//     return {
+//       firstName: firstName,
+//       lastName: lastName
+//     }
+//   }
+function createInstructor(firstName, lastName) {
+    return {
+        firstName,
+        lastName
+    };
+}
+
+//refactor into ES2015
+// var favoriteNumber = 42;
+
+// var instructor = {
+//   firstName: "Colt"
+// }
+
+// instructor[favoriteNumber] = "That is my favorite!"
+// function instructorsNum(firstName, favoriteNumber) {
+//     return {
+//         firstName,
+//         favoriteNumber
+//     };
+// } Solution:
+let favoriteNumber = 42;
+const instructorNum = {
+    firstName: "Colt",
+    [favoriteNumber]: "That is my favorite!"
+}
+
+//refactor into ES2015
+// var instructor = {
+//     firstName: "Colt",
+//     sayHi: function(){
+//       return "Hi!";
+//     },
+//     sayBye: function(){
+//       return this.firstName + " says bye!";
+//     }
+//   }
+const instructor = {
+    firstName: "Colt",
+    sayHi() {
+        return "Hi!";
+    },
+    sayBye() {
+        return this.firstName + " says bye!";
+    }
+}
+
+// Write a function which generates an animal object. The function should accepts 3 arguments:
+
+// - species: the species of animal (‘cat’, ‘dog’)
+// - verb: a string used to name a function (‘bark’, ‘bleet’)
+// - noise: a string to be printed when above function is called (‘woof’, ‘baaa’)
+
+// Use one or more of the object enhancements we’ve covered.
+function createAnimal(species, verb, noise) {
+    return {
+        species,
+        [verb]() {
+            return noise;
+        },
+    };
+}
